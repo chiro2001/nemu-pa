7efb4f8b0daf68390cc3e61c0e2edcef25183e13
tracer-ics2021 <tracer@njuics.org>
2021-12-17 12:00:50 +0800

    >  compile 200110619    Static hostname: Chiro-wsl          Icon name: computer-container            Chassis: container         Machine ID: c7d27d122a3c409aa2be5e8a07401df1            Boot ID: 87c1142fa50e467f92f45f519b5170df     Virtualization: wsl   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.10.60.1-microsoft-standard-WSL2       Architecture: x86-64  12:00:50 up  6:21,  7 users,  load average: 0.23, 0.26, 0.22

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -4,3 +4,16 @@
 def_EHelper(lui) { rtl_li(s, ddest, id_src1->imm); }
 def_EHelper(add) { rtl_add(s, ddest, id_src1->preg, id_src2->preg); }
 def_EHelper(addi) { rtl_addi(s, ddest, id_src1->preg, id_src2->imm); }
+def_EHelper(sub) { rtl_sub(s, ddest, id_src1->preg, id_src2->preg); }
+def_EHelper(xor) { rtl_xor(s, ddest, id_src1->preg, id_src2->preg); }
+def_EHelper(xori) { rtl_xori(s, ddest, id_src1->preg, id_src2->imm); }
+def_EHelper(or) { rtl_or(s, ddest, id_src1->preg, id_src2->preg); }
+def_EHelper(ori) { rtl_ori(s, ddest, id_src1->preg, id_src2->imm); }
+def_EHelper(and) { rtl_and(s, ddest, id_src1->preg, id_src2->preg); }
+def_EHelper(andi) { rtl_andi(s, ddest, id_src1->preg, id_src2->imm); }
+def_EHelper(sll) { rtl_sll(s, ddest, id_src1->preg, id_src2->preg); }
+def_EHelper(slli) { rtl_slli(s, ddest, id_src1->preg, id_src2->imm); }
+def_EHelper(srl) { rtl_srl(s, ddest, id_src1->preg, id_src2->preg); }
+def_EHelper(srli) { rtl_srli(s, ddest, id_src1->preg, id_src2->imm); }
+def_EHelper(sra) { rtl_sra(s, ddest, id_src1->preg, id_src2->preg); }
+def_EHelper(srai) { rtl_srai(s, ddest, id_src1->preg, id_src2->imm); }

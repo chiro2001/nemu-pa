628c368b8ff95d266a098d5116083dba90fed143
tracer-ics2021 <tracer@njuics.org>
2021-12-17 20:15:28 +0800

    >  compile 200110619    Static hostname: Chiro-wsl          Icon name: computer-container            Chassis: container         Machine ID: c7d27d122a3c409aa2be5e8a07401df1            Boot ID: 87c1142fa50e467f92f45f519b5170df     Virtualization: wsl   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.10.60.1-microsoft-standard-WSL2       Architecture: x86-64  20:15:28 up 12:24, 10 users,  load average: 0.26, 0.35, 0.27

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -1,10 +1,7 @@
 #include "rtl-basic.h"
 def_EHelper(lui) { rtl_li(s, ddest, id_src1->imm); }
 def_EHelper(add) { rtl_add(s, ddest, id_src1->preg, id_src2->preg); }
-def_EHelper(addi) {
-  Log("addi");
-  rtl_addi(s, ddest, id_src1->preg, id_src2->imm);
-}
+def_EHelper(addi) { rtl_addi(s, ddest, id_src1->preg, id_src2->imm); }
 def_EHelper(sub) { rtl_sub(s, ddest, id_src1->preg, id_src2->preg); }
 def_EHelper(xor) { rtl_xor(s, ddest, id_src1->preg, id_src2->preg); }
 def_EHelper(xori) { rtl_xori(s, ddest, id_src1->preg, id_src2->imm); }
@@ -24,9 +21,10 @@ def_EHelper(srai) { rtl_srai(s, ddest, id_src1->preg, id_src2->imm); }
 def_EHelper(jalr) {
   static rtlreg_t t, target;
   static rtlreg_t zero = 0;
+  Log("JUMP >> " FMT_WORD, target);
+  
   t = (rtlreg_t)(s->pc + 4);
   target = *id_src1->preg + imm_sext32(id_src2->imm, 12);
-  Log("JUMP >> " FMT_WORD, target);
   rtl_jr(s, &target);
   rtl_addi(s, ddest, &zero, t);
 }

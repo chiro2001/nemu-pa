da079b25515406d391a03904fe2d923015751965
Chiro Liang <chiro2001@163.com>
2021-12-19 22:38:47 +0800

    add: *printf

--- abstract-machine/klib/include/klib.h
+++ abstract-machine/klib/include/klib.h
@@ -5,6 +5,10 @@
 #include <stddef.h>
 #include <stdarg.h>
 
+#define _putchar putch
+
+#include "printf.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -32,11 +36,11 @@ int    abs       (int x);
 int    atoi      (const char *nptr);
 
 // stdio.h
-int    printf    (const char *format, ...);
-int    sprintf   (char *str, const char *format, ...);
-int    snprintf  (char *str, size_t size, const char *format, ...);
-int    vsprintf  (char *str, const char *format, va_list ap);
-int    vsnprintf (char *str, size_t size, const char *format, va_list ap);
+// int    printf    (const char *format, ...);
+// int    sprintf   (char *str, const char *format, ...);
+// int    snprintf  (char *str, size_t size, const char *format, ...);
+// int    vsprintf  (char *str, const char *format, va_list ap);
+// int    vsnprintf (char *str, size_t size, const char *format, va_list ap);
 
 // assert.h
 #ifdef NDEBUG

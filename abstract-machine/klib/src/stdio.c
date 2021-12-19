a2855b91d09fe5ad316e26bbe1ba27c056b2453b
Chiro Liang <chiro2001@163.com>
2021-12-19 22:38:47 +0800

    add: *printf

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,31 +1,10 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  // TODO: Implememt klib
-  // panic("Not implemented");
-  putch(fmt[0]);
-  return 0;
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
+// Implememted by printf.c
 
 #endif

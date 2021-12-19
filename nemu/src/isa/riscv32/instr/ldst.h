69689542784c3961b21f7dc537c8dfaf2b90168d
Chiro Liang <chiro2001@163.com>
2021-12-19 21:49:49 +0800

    done tests; todo: klib

--- nemu/src/isa/riscv32/instr/ldst.h
+++ nemu/src/isa/riscv32/instr/ldst.h
@@ -1,5 +1,5 @@
 def_EHelper(lw) { rtl_lm(s, ddest, dsrc1, id_src2->imm, 4); }
-def_EHelper(sw) { rtl_sm(s, ddest, dsrc1, id_src2->imm, 4); }
+
 def_EHelper(lb) {
   rtl_lm(s, ddest, dsrc1, id_src2->imm, 1);
   *ddest = imm_sext32(*ddest & 0xFF, 8);
@@ -18,6 +18,17 @@ def_EHelper(lhu) {
   Log("read " FMT_WORD " -> " FMT_WORD, *ddest, *ddest & 0xFFFF);
   *ddest = *ddest & 0xFFFF;
 }
-
-def_EHelper(sb) { rtl_sm(s, ddest, dsrc1, id_src2->imm & 0xFF, 1); }
-def_EHelper(sh) { rtl_sm(s, ddest, dsrc1, id_src2->imm & 0xFFFF, 2); }
+// sw rs2, offset(rs1)   //M[x[rs1] + sext(offset) = x[rs2][31: 0]
+def_EHelper(sw) { rtl_sm(s, ddest, dsrc1, id_src2->imm, 4); }
+// sb rs2, offset(rs1)   //M[x[rs1] + sext(offset)] = x[rs2][7: 0]
+def_EHelper(sb) {
+  static rtlreg_t target;
+  target = *ddest & 0xFF;
+  rtl_sm(s, &target, dsrc1, id_src2->imm, 1);
+}
+// def_EHelper(sh) { rtl_sm(s, ddest, dsrc1, id_src2->imm & 0xFFFF, 2); }
+def_EHelper(sh) {
+  static rtlreg_t target;
+  target = *ddest & 0xFFFF;
+  rtl_sm(s, &target, dsrc1, id_src2->imm, 2);
+}

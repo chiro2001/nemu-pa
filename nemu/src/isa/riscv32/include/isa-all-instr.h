8501118be926f4df0d48589d495d8e83148b6271
Chiro Liang <chiro2001@163.com>
2021-12-19 19:02:19 +0800

    arithimm.S passed

--- nemu/src/isa/riscv32/include/isa-all-instr.h
+++ nemu/src/isa/riscv32/include/isa-all-instr.h
@@ -4,9 +4,10 @@
 
 #define INSTR_NULLARY(f) f(inv) f(nemu_trap)
 #define INSTR_UNARY(f)
-#define INSTR_BINARY(f)                                                    \
-  f(add) f(addi) f(sub) f(lui) f(xor) f(xori) f(or) f(ori) f(and) \
-      f(andi) f(sll) f(slli) f(srl) f(srli) f(sra) f(srai)
+#define INSTR_BINARY(f)                                                   \
+  f(add) f(addi) f(sub) f(lui) f(xor) f(xori) f(or) f(ori) f(and) f(andi) \
+      f(sll) f(slli) f(srl) f(srli) f(sra) f(srai) f(slt) f(slti) f(sltu) \
+          f(sltiu)
 #define INSTR_TERNARY(f) f(lw) f(sw)
 
 def_all_EXEC_ID();

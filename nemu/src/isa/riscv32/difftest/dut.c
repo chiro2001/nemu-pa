721648049d984637db7145e0ac17419234894114
Chiro Liang <chiro2001@163.com>
2021-12-19 20:03:32 +0800

    jump2.S passed

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -4,7 +4,7 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   bool reg_diffs[32];
-  bool same = true;
+  bool same = cpu.pc == ref_r->pc;
   memset(reg_diffs, 0, sizeof(bool) * 32);
   for (size_t i = 0; i < 32; i++) {
     if (ref_r->gpr[i]._32 != gpr(i)) {

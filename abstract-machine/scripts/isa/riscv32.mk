39fa89f14dc61950aece83a63f9c40aecf56a0bd
Chiro Liang <chiro2001@163.com>
2021-12-21 16:14:38 +0800

    trying to debug riscv32

---
+++ abstract-machine/scripts/isa/riscv32.mk
@@ -0,0 +1,5 @@
+CROSS_COMPILE := $(CONFIG_EXT_RISCV_CROSS)
+COMMON_FLAGS  := -fno-pic -march=rv32im -mabi=ilp32
+CFLAGS        += $(COMMON_FLAGS) -static
+ASFLAGS       += $(COMMON_FLAGS) -O0
+LDFLAGS       += -melf32lriscv
